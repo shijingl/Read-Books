ce1d59d1c770b41a197989cca01e9bc0276c292b
Shijing Liu <liushijing123@gmail.com>
2020-02-26 14:52:30 -0500

    first commit

---
+++ build/precache-manifest.d2381f0320c73f57208a75d982c0326a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2bbcc4832bef5a5651f8",
+    "url": "/static/js/main.2bbcc483.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "2bbcc4832bef5a5651f8",
+    "url": "/static/css/main.1bc0b236.chunk.css"
+  },
+  {
+    "revision": "3a5152a62b55e1042ea37a901efe4cec",
+    "url": "/index.html"
+  }
+];
