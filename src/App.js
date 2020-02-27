ddf29d02a924d7b1e34fd312d2da17f7e55c47e2
Shijing Liu <liushijing123@gmail.com>
2020-02-27 17:39:04 -0500

    install react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
   }
 
+  render() {
+    return (
+      <div className="app">
+      </div>
+    )
+  }
+  /*
   render() {
     return (
       <div className="app">
@@ -21,14 +22,7 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
+                {}
                 <input type="text" placeholder="Search by title or author"/>
 
               </div>
@@ -201,6 +195,9 @@ class BooksApp extends React.Component {
       </div>
     )
   }
+
+  */
+
 }
 
 export default BooksApp
